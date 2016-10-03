2801781d8ddc72e1a16f5b54be0fd77e7f30dcce
Igor Borodikhin <iborodikhin@gmail.com>
2016-10-03 16:34:10 +0600

    Added function for checking if the integer is in slice.

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -14,3 +14,18 @@ func StringInSlice(haystack []string, needle string) (result int) {
 
 	return
 }
+
+// Checks if integer exists in a slice
+// Returns the index for needle if it is found in the slice, -1 otherwise.
+func IntInSlice(haystack []int, needle int) (index int) {
+	index = -1
+
+	for k, v := range haystack {
+		if v == needle {
+			index = k
+			break
+		}
+	}
+
+	return
+}
