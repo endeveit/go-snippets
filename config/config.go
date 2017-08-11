dbdc14447f0142dcf6fbea09d66b982794a4b1d7
Igor Borodikhin <iborodikhin@gmail.com>
2017-08-11 12:53:54 +0600

    Dump config to string

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+	"strings"
 	"sync"
 
 	hc "github.com/endeveit/go-snippets/cli"
@@ -10,6 +12,10 @@ import (
 var (
 	conf *c.Config
 	once sync.Once
+)
+
+const (
+	SECTION_DEFAULT = "DEFAULT"
 )
 
 func initConfig(paths []string) {
@@ -34,4 +40,25 @@ func Instance(paths ...string) *c.Config {
 	initConfig(paths)
 
 	return conf
+}
+
+// Dumps configuration to string
+func Dump(conf *c.Config) string {
+	var result []string
+
+	for _, section := range conf.Sections() {
+		if section != SECTION_DEFAULT {
+			result = append(result, "["+section+"]")
+		}
+
+		if options, err := conf.SectionOptions(section); err == nil {
+			for _, option := range options {
+				if value, err := conf.RawString(section, option); err == nil {
+					result = append(result, fmt.Sprintf("%s = %s\r\n", option, value))
+				}
+			}
+		}
+	}
+
+	return strings.Join(result, "\r\n")
 }
